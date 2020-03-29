a1e35d2d046520adfd483fc576ffe775a32bb12c
saregbesola <35005799+saregbesola@users.noreply.github.com>
2020-03-29 00:45:09 -0400

    Update ConvertWordDocument2AspxPage.psm1

--- ConvertWordDocument2AspxPage/ConvertWordDocument2AspxPage.psm1
+++ ConvertWordDocument2AspxPage/ConvertWordDocument2AspxPage.psm1
@@ -329,18 +329,7 @@ using DocumentFormat.OpenXml.Wordprocessing;
                 destination.Write(buffer, 0, bytesRead);
             } while (bytesRead != 0);
         }
-        private static string DecodeFrom64(string encodedData)
-        {
-            System.Text.UTF8Encoding encoder = new System.Text.UTF8Encoding();
-            System.Text.Decoder utf8Decode = encoder.GetDecoder();
-            byte[] todecode_byte = Convert.FromBase64String(encodedData);
-            int charCount = utf8Decode.GetCharCount(todecode_byte, 0, todecode_byte.Length);
-            char[] decoded_char = new char[charCount];
-            utf8Decode.GetChars(todecode_byte, 0, todecode_byte.Length, decoded_char, 0);
-            string result = new String(decoded_char);
-            return result;
-        }
-
+        
         private static void SendEmails(string _To, string _subject, string _textBody,string userName, string password)
         {
             // Create and build a new MailMessage object
